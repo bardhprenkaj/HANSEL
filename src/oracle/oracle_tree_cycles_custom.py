634d128635f5cb18f9ac43f0b956c23dc2de3fb8
Bardh Prenkaj <bardhprenkaj95@gmail.com>
2023-07-01 18:22:59 +0200

    custom tree cycles oracle

--- src/oracle/oracle_tree_cycles_custom.py
+++ src/oracle/oracle_tree_cycles_custom.py
@@ -14,19 +14,12 @@ class TreeCyclesCustomOracle(Oracle):
         pass
 
     def _real_predict(self, data_instance):
-        try:
-            nx.find_cycle(data_instance.graph, orientation='ignore')
-            return 1
-        except nx.exception.NetworkXNoCycle:
-            return 0
+        cycles = nx.cycle_basis(data_instance.graph)
+        return 1 if len(cycles) > 0 else 0
         
     def _real_predict_proba(self, data_instance):
-        # softmax-style probability predictions
-        try:
-            nx.find_cycle(data_instance.graph, orientation='ignore')
-            return np.array([[1,0]])
-        except nx.exception.NetworkXNoCycle:
-            return np.array([[0,1]])
+        cycle_exists = self._real_predict(data_instance)
+        return np.array([[1,0]]) if cycle_exists else np.array([[0,1]])
 
     def embedd(self, instance):
         return instance
