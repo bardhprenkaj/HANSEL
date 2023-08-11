429a6362348eeabd8590e458e240c8717c7c8fa2
Bardh Prenkaj <bardhprenkaj95@gmail.com>
2023-08-11 12:19:40 +0200

    more prints!

--- src/dataset/dynamic_graphs/dataset_btc_alpha.py
+++ src/dataset/dynamic_graphs/dataset_btc_alpha.py
@@ -28,7 +28,7 @@ class BTCAlpha(DynamicDataset):
     def read_csv_file(self, dataset_path):
         # read the number of vertices in for each simplex
         ratings = pd.read_csv(os.path.join(dataset_path, 'soc-bitcoin.edges'), header=None, names=['source','target','rating','time'])
-        print(ratings.head())
+        print(ratings.head(50))
         ratings.time = ratings.time.apply(lambda x : pd.to_datetime(x, unit='s'))
         ratings['year'] = ratings.time.apply(lambda x : x.year)
         # retain only desired history
