3da9722a7e9f92350ef4046526b2abbbc80b3632
Bardh Prenkaj <bardhprenkaj95@gmail.com>
2024-02-02 17:01:17 +0100

    dataset stats updated

--- dataset_stats.py
+++ dataset_stats.py
@@ -1,14 +1,24 @@
+from src.dataset.dataset_factory import DatasetFactory
 from src.data_analysis.data_analyzer import DataAnalyzer
 
+import numpy as np
 # data_store_path = './data/datasets/'
 # dtan = DataAnalyzer('./output', './stats')
 
-data_store_path = './data/datasets'
-dtan = DataAnalyzer('./output/steel/BBBP', './stats/steel/')
+data_store_path = './data/datasets/'
+dtan = DataAnalyzer('./output/steel/BBBP', './stats/btc_otc/')
 
 
 datasets =[
-    {"name": "tree-cycles", "parameters": {"n_inst": 500, "n_per_inst": 32, "n_in_cycles": 7} }
+    {
+             "name": "btc_otc", 
+            "parameters": {
+                "begin_time": 2011,
+                "end_time": 2015,
+                "filter_min_graphs": 4,
+                "number_of_communities": 4
+            } 
+        }
 ]
 
 dtan.get_datasets_stats(datasets, data_store_path)
